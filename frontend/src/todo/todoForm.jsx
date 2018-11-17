db9ff345cbfd81ba52dad8345420149299082e52
Lucas <lucascavalcante@gmail.com>
2018-11-16 22:30:08 -0300

    add and delete a task

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div>
-        <h1>Form</h1>
+        <div className='todoForm' role='form'>
+            <Grid cols='12 9 10'>
+                <input id='description'
+                    className='form-control'
+                    placeholder='Add a task here'
+                    onChange={props.handleChange}
+                    value={props.description} />
+            </Grid>
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+            </Grid>
+        </div>
     </div>
 )
